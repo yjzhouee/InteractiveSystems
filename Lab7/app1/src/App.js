d2eb72b2f2fcba6097d519a2a1c68f85aa5992d9
yjzhouee <yjzhou@tlu.ee>
2024-04-25 19:54:42 +0300

    Lab7_inClass
    Movie React app.

---
+++ Lab7/app1/src/App.js
@@ -0,0 +1,41 @@
+//import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import APIKey from './config';
+import './App.css';
+import Movie from './Component/Movie'; 
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=';
+
+
+
+function App() {
+
+  const [movieResults, setMovieResults] = useState([]); 
+  //const movieResults = [1,2,3]; 
+
+  useEffect( () => {
+    fetch(APIURL + APIKey)
+    .then( (resp) => resp.json() ) 
+    .then( (data) => {
+      console.log(data)
+      setMovieResults(data.results)
+    }
+    )
+  }
+  ,[]) 
+
+
+  return (
+    <>
+    {movieResults.map( (element,index) => (<Movie key={index}/>)
+
+    )}
+    </>
+  );
+}
+
+export default App;
+
+
+ // new app - cd \sdf\ - npx create-react-app app1
+ //react + food exercise. - same stage. [result in array.]
+ //plus: continue this class. 
---
+++ Lab7/app1/src/Component/Movie.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+
+
+function Movie() {
+    return (
+     <div>This is my movie.</div>
+     
+    );
+  }
+  
+export default Movie; 
