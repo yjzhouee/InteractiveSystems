fafb51b4c7fabc27cd0d8896ab139fd63d24993d
yjzhouee <yjzhou@tlu.ee>
2024-05-02 19:51:04 +0300

    Lab7_2ndMay_inClass
    Continue the last session.

--- Lab7/app1/src/App.js
+++ Lab7/app1/src/App.js
@@ -4,13 +4,16 @@ import APIKey from './config';
 import './App.css';
 import Movie from './Component/Movie'; 
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=';
+const SearchURL = 'https://api.themoviedb.org/3/search/movie?api_key=';
 
 
 
 function App() {
 
   const [movieResults, setMovieResults] = useState([]); 
+  const [searchTerm, setSearchTerm] = useState('');
   //const movieResults = [1,2,3]; 
+
 
   useEffect( () => {
     fetch(APIURL + APIKey)
@@ -24,12 +27,39 @@ function App() {
   }
   ,[]) 
 
+  const handleOnSubmit = (e) =>{
+    e.preventDefault();
 
+    if(searchTerm){
+      fetch(SearchURL + APIKey + "&query=" +searchTerm)
+      .then( (resp) => resp.json() ) 
+      .then( (data) => {
+        console.log(data)
+        setMovieResults(data.results)      
+    })
+    }
+    }
+
+  const handleOnChange = (e) => {
+    console.log(e.target.value)
+    setSearchTerm(e.target.value)
+  }
   return (
     <>
-    {movieResults.map( (element,index) => (<Movie key={index}/>)
+    <header>
+      <form onSubmit={handleOnSubmit}>
+        <input className='search' type='search' placeholder='Search' value={searchTerm} onChange={handleOnChange}></input>
+
+      </form>
+
+    </header>
+
+    <div className='movie-container'>
+    {movieResults.map( (element) => (<Movie key={element.id} {...element}/>)
 
     )}
+    
+    </div>
     </>
   );
 }
@@ -37,6 +67,23 @@ function App() {
 export default App;
 
 
- // new app - cd \sdf\ - npx create-react-app app1
- //react + food exercise. - same stage. [result in array.]
- //plus: continue this class. 
+ //homework 
+
+ //1
+ //conditional rendering - if value of certain item is missing, change the content.
+ //if..else -> the value is 'null' (picture not shown), change another picture:  
+ //save the extra image in an extra folder. 
+ //syntax (alike): <poster_path?> <poster_path:> <sth_else> 
+
+ //2
+ //rating number above 7 > green; below 7 > yellow, below 5> red. 
+
+ //3 
+ /* header should make its own component. like a navigation. 
+ <header>
+ <form onSubmit={handleOnSubmit}>
+   <input className='search' type='search' placeholder='Search' value={searchTerm} onChange={handleOnChange}></input>
+
+ </form>
+
+ */
--- Lab7/app1/src/Component/Movie.js
+++ Lab7/app1/src/Component/Movie.js
@@ -1,12 +1,26 @@
 import React from "react"; 
+import "./Movie.css"; 
+const ImageURL = 'https://image.tmdb.org/t/p/w500/';
 
-
-
-function Movie() {
+function Movie({poster_path, title, overview, vote_average}) {
     return (
-     <div>This is my movie.</div>
+     <div className = "movie"> 
+        <img src = {ImageURL + poster_path} alt = {title}></img>
+        <div className = "movie-info">
+          <h1>{title}</h1>
+          <span>{vote_average}</span>
+        </div>
+      <div className="movie-overview">
+        <h2>overview</h2>
+        <p>{overview}</p>
+
+      </div>
+
+     </div>
      
     );
   }
   
 export default Movie; 
+
+//<div className = movie-container>
